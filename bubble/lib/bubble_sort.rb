1720689a9e6b77387ca7939f2c51a046dc72d1fe
Nate <nategiesing@gmail.com>
2017-07-22 14:38:46 -0600

    working bubble_sort

--- bubble/lib/bubble_sort.rb
+++ bubble/lib/bubble_sort.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class BubbleSort
-
+  def sort(arg)
+    arg.length.times do
+    arg.each_with_index do |item, index|
+      previous_value = arg[index]
+      current_value = arg[index + 1]
+        if current_value != nil && previous_value > current_value
+          arg[index] = current_value
+          arg[index+1] = previous_value
+        end
+      end
+    end
+    p arg
+  end
 end
+
+# sorter = BubbleSort.new
+# # => <BubbleSort:0x007f81a19e94e8>
+# sorter.sort(["d", "b", "a", "c"])
+# # =>["a", "b", "c", "d"]
--- bubble/test/bubble_sort_test.rb
+++ bubble/test/bubble_sort_test.rb
@@ -6,8 +6,24 @@ require "./lib/bubble_sort"
 class BubbleSortTest < Minitest::Test
 
   def test_it_exists
-    bubble = BubbleSort.new
+    sorter = BubbleSort.new
 
-    assert_instance_of BubbleSort, bubble
+    assert_instance_of BubbleSort, sorter
+  end
+
+  def test_it_returns_in_the_right_order_with_letter
+    sorter = BubbleSort.new
+    expected = ["a", "b", "c", "d"]
+    actual = sorter.sort(["d", "b", "a", "c"])
+
+    assert_equal expected, actual
+  end
+
+  def test_it_returns_in_the_right_order_with_num
+    sorter = BubbleSort.new
+    expected = [0, 1, 2, 3, 4, 5, 6]
+    actual = sorter.sort([2,1,0,6,4,5,3])
+
+    assert_equal expected, actual
   end
 end
